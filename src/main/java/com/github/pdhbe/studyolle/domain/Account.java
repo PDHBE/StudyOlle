387dfe9b651d6b1355187cbc1bab6af00e2f5ddd
PDHBE <ehgusdl68@gmail.com>
2021-06-15 15:59:10 +0900

    feat: add Account domain class

---
+++ src/main/java/com/github/pdhbe/studyolle/domain/Account.java
@@ -0,0 +1,40 @@
+package com.github.pdhbe.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @NoArgsConstructor @AllArgsConstructor
+public class Account {
+    @Id @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+    private String password;
+
+    private boolean emailVerified;
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+    private String url;
+    private String occupation;
+    private String location;
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean AlarmByEmailStudyCreated;
+    private boolean AlarmByWebStudyCreated;
+
+    private boolean AlarmByEmailStudyEnrollmentResult;
+    private boolean AlarmByWebStudyEnrollmentResult;
+
+    private boolean AlarmByEmailStudyUpdated;
+    private boolean AlarmByWebStudyUpdated;
+}
