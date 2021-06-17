9f24fd01ba443379c005ce7e25f2f5aa57e79e59
PDHBE <ehgusdl68@gmail.com>
2021-06-17 16:46:21 +0900

    feat: add SignUpFormSubmit

--- src/main/java/com/github/pdhbe/studyolle/account/AccountController.java
+++ src/main/java/com/github/pdhbe/studyolle/account/AccountController.java
@@ -1,6 +1,9 @@
 package com.github.pdhbe.studyolle.account;
 
+import com.github.pdhbe.studyolle.domain.Account;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -16,23 +19,45 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class AccountController {
     private final SignUpFormValidator signUpFormValidator;
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
 
     @InitBinder(value = "signUpFormDto")
-    public void initBinder(WebDataBinder webDataBinder){
+    public void initBinder(WebDataBinder webDataBinder) {
         webDataBinder.addValidators(signUpFormValidator);
     }
 
     @GetMapping("/sign-up")
-    public String signUpForm(Model model){
-        model.addAttribute("signUpFormDto",new SignUpFormDto());
+    public String signUpForm(Model model) {
+        model.addAttribute("signUpFormDto", new SignUpFormDto());
         return "account/sign-up";
     }
 
     @PostMapping("/sign-up")
-    public String signUpSubmit(@Valid @ModelAttribute SignUpFormDto signUpFormDto, Errors errors){
-        if(errors.hasErrors()){
+    public String signUpSubmit(@Valid @ModelAttribute SignUpFormDto signUpFormDto, Errors errors) {
+        if (errors.hasErrors()) {
             return "account/sign-up";
         }
+
+        Account account = Account.builder()
+                .nickname(signUpFormDto.getNickname())
+                .email(signUpFormDto.getEmail())
+                .password(signUpFormDto.getPassword()) // 추후에 encoding 작업 추가
+                .AlarmByWebStudyCreated(true)
+                .AlarmByWebStudyUpdated(true)
+                .AlarmByWebStudyEnrollmentResult(true)
+                .build();
+
+        Account savedAccount = accountRepository.save(account);
+
+        savedAccount.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(savedAccount.getEmail());
+        mailMessage.setSubject("스터디 올레, 회원 가입 인증");
+        mailMessage.setText("/check-email-token?token=" + savedAccount.getEmailCheckToken() +
+                "&email=" + savedAccount.getEmail());
+        javaMailSender.send(mailMessage);
+
         return "redirect:/";
     }
 }
