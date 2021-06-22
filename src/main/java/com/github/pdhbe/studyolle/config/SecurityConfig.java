00ff8b01364a56384addf4b21aaf498461b82ccd
PDHBE <ehgusdl68@gmail.com>
2021-06-22 13:35:07 +0900

    프론트엔드 라이브러리 설정

--- src/main/java/com/github/pdhbe/studyolle/config/SecurityConfig.java
+++ src/main/java/com/github/pdhbe/studyolle/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.github.pdhbe.studyolle.config;
 
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,13 +14,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .mvcMatchers("/","/sign-up","/check-email-token").permitAll()
+                .mvcMatchers("/","/login","/sign-up","/check-email","/check-email-token",
+                        "/email-login","/check-email-login","/login-link").permitAll()
+                .mvcMatchers(HttpMethod.GET,"/profile/*").permitAll()
                 .anyRequest().authenticated();
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring()
+                .mvcMatchers("/node_modules/**")
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
